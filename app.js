accfc24019db5f645b58a0c6b87237512796a010
Adan Nugraha <contact.ardana@gmail.com>
2021-09-30 23:22:13 +0700

    study 1

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+// EJS = Embedded JavaScript
+
+// menspesifikasikan folder yang menyimpan file CSS dan gambar
+app.use(express.static('public'));
+
+
+// menambahkan route ke top.ejs melalui root
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
+});
+
+// menambahkan route ke index.ejs di folder index
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000);
