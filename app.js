64b6ac5604b5377b05800d5f2a6fe36d08ed0568
Adan Nugraha <contact.ardana@gmail.com>
2021-10-01 00:07:33 +0700

    study II no 4

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
-
 // EJS = Embedded JavaScript
 
 // menspesifikasikan folder yang menyimpan file CSS dan gambar
 app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+  host: 'localhost:3306',
+  user: 'adan',
+  password: 'adanrootsql123',
+  database: 'list_app'
+});
 
 // menambahkan route ke top.ejs melalui root
 app.get('/', (req, res) => {
@@ -15,7 +20,14 @@ app.get('/', (req, res) => {
 
 // menambahkan route ke index.ejs di folder index
 app.get('/index', (req, res) => {
-  res.render('index.ejs');
+  // query sql
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      // Teruskan object sebagai argument ke-2 res.render
+      res.render('index.ejs', {items: results});
+    }
+  );
 });
 
 app.listen(3000);
